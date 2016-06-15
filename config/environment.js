1386153e8208412023ca44b3f6edd631c7f9363b
jd <jldjld503@gmail.com>
2016-06-15 15:42:14 -0700

    skeleton created and multiple compoenents and routes

---
+++ config/environment.js
@@ -0,0 +1,54 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'bookmarket',
+    environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: {
+      apiKey: "AIzaSyAjJNbgS9kqXTC8qV1ZOVtdKyHGhQPj82E",
+      authDomain: "project-7899838841510983056.firebaseapp.com",
+      databaseURL: "https://project-7899838841510983056.firebaseio.com",
+      storageBucket: "project-7899838841510983056.appspot.com",
+    },
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
---
+++ tests/unit/helpers/book-cost-test.js
@@ -0,0 +1,10 @@
+import { bookCost } from 'bookmarket/helpers/book-cost';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | book cost');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = bookCost([42]);
+  assert.ok(result);
+});
