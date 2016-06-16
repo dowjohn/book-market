d350798147383cdd1ecb337e51936df01d346aeb
jd <Guest@epicodus-68.local>
2016-06-16 16:46:19 -0700

    d

--- app/components/shopping-cart.js
+++ app/components/shopping-cart.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
 
   shoppingCart: Ember.inject.service(),
   total: Ember.computed(function(){
+    console.log('is working');
     return this.get('shoppingCart').getTotal();
-  })
+  }),
+
+  actions: {
+    removeFromCart(book) {
+      this.get("shoppingCart").remove(book);
+    }
+  }
 });
